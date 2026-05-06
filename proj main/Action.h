d86ae4718a7e4c29e6d36289e26185ffb20cac45
Abdelrahman Eletreby <abdelrahman.hassan063@eng-std.edu.eg>
2026-05-06 19:09:15 +0300

    committt!!!!

--- proj main/Action.h
+++ proj main/Action.h
@@ -1,7 +1,7 @@
 #pragma once
 
 class Restaurant; // Forward declaration of Restaurant class
-
+//hello rana, how are? what did you eat today? i hope you had a great day! if you need any help with your code, feel free to ask.
 class Action {
 protected:
     int ActionTime;    // The timestep this action occurs (TS)
